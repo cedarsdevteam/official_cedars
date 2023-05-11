a2b0fa5c50062e40cc26b6b7106639c5877e8d16
Ng-Harry <ngereharry5@gmail.com>
2023-05-11 10:55:30 +0100

    remove front-end dir

--- cedars/settings.py
+++ cedars/settings.py
@@ -16,9 +16,6 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-wv6ovdw8-s*^#k&2-o6$os(i0ebi)j8-s9ha=fvdf)5fm-0+z0"
 
