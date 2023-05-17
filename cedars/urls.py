491d785b52fe5ef1a759dcf9fcbd6a3126edbf5e
cedarsdevteam <97524707+cedarsdevteam@users.noreply.github.com>
2023-05-17 22:16:00 +0100

    Update urls.py

--- cedars/urls.py
+++ cedars/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import TemplateView
+from . import views
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", TemplateView.as_view(template_name = "index.html"),
+    path("", views.index.html, name="index"),
     )
 ]
