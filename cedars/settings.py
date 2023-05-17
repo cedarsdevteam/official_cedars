9a228e50d0b417629be43184ccfa965dc095a6a0
cedarsdevteam <97524707+cedarsdevteam@users.noreply.github.com>
2023-05-17 22:49:30 +0100

    Update settings.py

--- cedars/settings.py
+++ cedars/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR, 'cedars-productivity-centre/build/static']
 STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
-STATIC_ROOT = [BASE_DIR, 'staticfiles']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
