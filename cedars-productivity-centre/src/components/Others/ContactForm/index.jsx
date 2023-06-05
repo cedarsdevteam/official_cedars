769249679662d94ba4ed1f3af8ec83cadcdac7fc
S0DIENYE <peppledavid29@gmail.com>
2023-06-05 17:44:51 +0100

    loader redefined

--- cedars-productivity-centre/src/components/Others/ContactForm/index.jsx
+++ cedars-productivity-centre/src/components/Others/ContactForm/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import AutohideToast from './toast';
 import './c-style.css';
 
 export class ContactForm extends Component {
@@ -6,57 +7,20 @@ export class ContactForm extends Component {
         error: null,
     };
 
-    // validateNames = () => {
-    //     const names = document.querySelectorAll('[validate="name"]');
-    //     // regexp condition = names must not be 2 < 15
-    //     const re = /^[a-z]{2,}$/i;
-
-    //     names.forEach(name => {
-
-    //         if(name)
-    //         if(!re.test(name.value)){
-    //             // throw error and set error state
-    //             name.classList.add('is-invalid')
-    //             this.state.error = true;
-    //             name.focus()
-
-    //         } else{
-    //             // success
-    //             name.classList.remove('is-invalid')
-    //             this.state.errDone = false;
-    //         }
-
-    //     })
-    // }
-
-    // validateEmail = () => {
-    //     const email = document.querySelector('[validate="email"]');
-    //     // regexp condition @mail !< 3, .com !< 2
-    //     const re = /^([a-z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
-
-    //     if(email)
-    //     if(!re.test(email.value)){
-    //         email.classList.add('is-invalid')
-    //         this.state.error = true;
-    //         email.focus()
-    //     } else{
-    //         email.classList.remove('is-invalid')
-    //         this.state.errDone = false;
-    //     }
-    // }
-    
     handleSubmit = (event) => {
         event.preventDefault();
         // Set alert {validation for empty user value}
-        // if(this.state.text === ''){
-        //     this.props.setAlert('Please enter something', 'light')
-        // } else{
+        if(this.state.text === '' || this.state.email === '' || this.state.message === ''){
+            // Call error toast and state
+            // this.props.setAlert('Please enter something', 'light')
+        } else{
+            // ** Success
+            // Passing the user data up through props
+            this.props.formDatas(this.state.text);
 
-        //     // Passing the user input up through props
-        //     this.props.searchUsers(this.state.text);
-    
-        //     this.setState({ text: '' })
-        // }
+            this.setState({ text: '', email: '', message: '' })
+        }
+
         console.log(this.state.text);
     }
 
@@ -65,39 +29,83 @@ export class ContactForm extends Component {
         this.setState({ [e.target.name]: e.target.value });
     }
 
-    // handleChange = handleChange.bind(this);
-    // handleSubmit = handleSubmit.bind(this);
-    
-    
     render() {
-      const {header, subText} = this.props;
-      
-    return (
-      <div>
-        <div className="header text-start mb-5">
-            <h1>{header}</h1>
-            <h5>{subText}</h5>
-        </div>
-        <form  onSubmit={this.handleSubmit}>
-            <input type="text" className="form-control" name="text" placeholder="Your name" 
-                    onChange={this.handleChange} value={this.state.text}/>
+        const { header, subText } = this.props;
 
-            <input type="email" name="email" className="form-control" placeholder="Enter your email address" 
-                   onChange={this.handleChange} value={this.state.email} />
+        return (
+            <div>
+                <div className="header text-start mb-5">
+                    <h1>{header}</h1>
+                    <h5>{subText}</h5>
+                </div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" className="form-control" name="text" placeholder="Your name"
+                        onChange={this.handleChange} value={this.state.text} />
 
-            <textarea className="form-control" cols="30" rows="10" name="message" placeholder="Your message"
-                    onChange={this.handleChange} value={this.state.message}></textarea>
+                    <input type="email" name="email" className="form-control" placeholder="Enter your email address"
+                        onChange={this.handleChange} value={this.state.email} />
 
-            <button className="get-involved-btn btn mt-2" type="submit">Get Involved</button>
-        </form>
-      </div>
-    )
-  }
+                    <textarea className="form-control" cols="30" rows="10" name="message" placeholder="Your message"
+                        onChange={this.handleChange} value={this.state.message}></textarea>
+
+                    <button className="get-involved-btn btn mt-2" type="submit">Get Involved</button>
+                </form>
+
+                <AutohideToast />
+            </div>
+        )
+    }
 }
 
 ContactForm.defaultProps = {
     header: 'Get in touch',
     subText: 'feel free to get in touch with us'
 };
+
+function validateForm(name, email, message) {
+
+    console.log(this.state.error, 'error state');
+
+    const validateNames = () => {
+        // regexp condition = names must not be 2 < 15
+        const re = /^[a-z]{2,}$/i;
+
+        if (!re.test(name)) {
+            // throw error and set error state
+            // init toast
+            // name.classList.add('is-invalid')
+            this.state.error = true;
+            name.focus()
+
+        } else {
+            // success
+            // init toast
+            // name.classList.remove('is-invalid')
+            this.state.error = false;
+        }
+    }
+
+    const validateEmail = () => {
+        // regexp condition @mail !< 3, .com !< 2
+        const re = /^([a-z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+        if (!re.test(email)) {
+            // init toast
+            // email.classList.add('is-invalid')
+            this.state.error = true;
+            email.focus()
+        } else {
+            // init toast
+            // email.classList.remove('is-invalid')
+            this.state.error = false;
+        }
+    }
+
+    const validateMessage = () => {
+        if(message.length > 2){
+            console.log('error');
+        }
+    }
+}
 
 export default ContactForm;
---
+++ cedars-productivity-centre/src/components/Others/ContactForm/toast.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Toast from 'react-bootstrap/Toast';
+
+function AutohideToast() {
+  const [show, setShow] = useState(false);
+
+  return (
+    <div>
+        <Col xs={6}>
+            <Toast onClose={() => setShow(true)} show={show} delay={3000} autohide>
+            <Toast.Header>
+                <strong className="me-auto">Bootstrap</strong>
+                <small>11 mins ago</small>
+            </Toast.Header>
+            <Toast.Body>Woohoo, you're reading this text in a Toast!</Toast.Body>
+            </Toast>
+        </Col>
+        
+        <Col xs={6}>
+            <Button onClick={() => setShow(true)}>Show Toast</Button>
+        </Col>
+    </div>
+  );
+}
+
+export default AutohideToast;
