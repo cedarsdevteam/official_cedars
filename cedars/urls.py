5d91b7947d8e1afcc88e6036be7ae4f2c9c51f63
Ng-Harry <ngereharry5@gmail.com>
2023-08-03 17:40:30 +0100

    update postgresql config

--- cedars/urls.py
+++ cedars/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/api/dashboard", admin.site.urls),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
     path("", TemplateView.as_view(template_name = "index.html")),
